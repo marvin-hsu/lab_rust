ed42d0bc7ae0c1a4a8de13a88fec59c38b7e5a97
Marvin Hsu <n26064074@gs.ncku.edu.tw>
2024-05-09 23:25:15 +0800

    refactor test

--- lab_optimistic_lock/src/lib.rs
+++ lab_optimistic_lock/src/lib.rs
@@ -1,10 +1,9 @@
 use byteorder::{BigEndian, ByteOrder};
-use sqlx::{Decode, Encode, Postgres, Type};
 use sqlx::encode::IsNull;
 use sqlx::error::BoxDynError;
-use sqlx::postgres::{PgArgumentBuffer, PgTypeInfo, PgValueFormat, PgValueRef};
 use sqlx::postgres::types::Oid;
-
+use sqlx::postgres::{PgArgumentBuffer, PgTypeInfo, PgValueFormat, PgValueRef};
+use sqlx::{Decode, Encode, Postgres, Type};
 
 // #[derive(sqlx::Type)]
 // #[sqlx(type_name = "xid")]
@@ -37,44 +36,19 @@ impl Decode<'_, Postgres> for Xid {
 
 #[cfg(test)]
 mod tests {
-    use sqlx::{Connection, postgres::PgConnection, types::Uuid};
-    use testcontainers::{RunnableImage, runners::AsyncRunner};
+    use sqlx::{postgres::PgConnection, types::Uuid, Connection};
+    use test_context::{test_context, AsyncTestContext};
+    use testcontainers::{runners::AsyncRunner, ContainerAsync, RunnableImage};
     use testcontainers_modules::postgres::Postgres;
 
     use crate::Xid;
 
+    #[test_context(PgContext)]
     #[tokio::test]
-    async fn it_works() {
-        let container = RunnableImage::from(
-            Postgres::default()
-                .with_db_name("test")
-                .with_user("test")
-                .with_password("1234"),
-        )
-            .with_tag("16.2-bullseye")
-            .start()
-            .await;
-
-        let connection_string = format!(
-            "postgres://test:1234@127.0.0.1:{}/test",
-            container.get_host_port_ipv4(5432).await
-        );
-
-        let mut conn = PgConnection::connect(connection_string.as_str())
+    async fn it_works(ctx: &mut PgContext) {
+        let mut conn = PgConnection::connect(ctx.connection_string.as_str())
             .await
             .expect("Connect to Postgres Fail.");
-
-        sqlx::query(
-            r#"
-            CREATE TABLE IF NOT EXISTS test_table (
-                id UUID PRIMARY KEY,
-                value VARCHAR
-            )
-            "#,
-        )
-            .execute(&mut conn)
-            .await
-            .expect("Create table fail.");
 
         let id = Uuid::new_v4();
 
@@ -84,33 +58,94 @@ mod tests {
             INSERT INTO test_table (id, value) VALUES ($1, $2)
             "#,
         )
-            .bind(id)
-            .bind("test")
-            .execute(&mut conn)
-            .await
-            .expect("Insert row fail.");
+        .bind(id)
+        .bind("test")
+        .execute(&mut conn)
+        .await
+        .expect("Insert row fail.");
 
-        let row: (Xid, ) = sqlx::query_as(
+        let row: Xid = sqlx::query_scalar(
             r#"
             SELECT xmin FROM test_table WHERE id = $1
             "#,
         )
-            .bind(id)
-            .fetch_one(&mut conn)
-            .await
-            .expect("Select row fail.");
+        .bind(id)
+        .fetch_one(&mut conn)
+        .await
+        .expect("Select row fail.");
 
-        let new: (Uuid, ) = sqlx::query_as(
+        // update the row without checking the xmin
+        _ = sqlx::query(
             r#"
-            SELECT id FROM test_table WHERE xmin = $1
+            UPDATE test_table SET value = $1 WHERE id = $2
             "#,
         )
-            .bind(row.0)
-            .fetch_one(&mut conn)
-            .await
-            .expect("Select row fail.");
+        .bind("test2")
+        .bind(id)
+        .execute(&mut conn)
+        .await
+        .expect("Update row fail.");
 
-        assert_eq!(new.0, id);
+        // update the row with checking the xmin
+        let affected_rows = sqlx::query(
+            r#"
+            UPDATE test_table SET value = $1 WHERE id = $2 AND xmin = $3
+            "#,
+        )
+        .bind("test3")
+        .bind(id)
+        .bind(row)
+        .execute(&mut conn)
+        .await
+        .expect("Update row fail.")
+        .rows_affected();
+
+        assert_eq!(affected_rows, 0);
+    }
+
+    #[allow(dead_code)]
+    struct PgContext {
+        pg_container: ContainerAsync<Postgres>,
+        connection_string: String,
+    }
+
+    impl AsyncTestContext for PgContext {
+        async fn setup() -> PgContext {
+            let pg_container = RunnableImage::from(
+                Postgres::default()
+                    .with_db_name("test")
+                    .with_user("test")
+                    .with_password("1234"),
+            )
+            .with_tag("16.2-bullseye")
+            .start()
+            .await;
+
+            let connection_string = format!(
+                "postgres://test:1234@127.0.0.1:{}/test",
+                pg_container.get_host_port_ipv4(5432).await
+            );
+
+            let mut conn = PgConnection::connect(connection_string.as_str())
+                .await
+                .expect("Connect to Postgres Fail.");
+
+            sqlx::query(
+                r#"
+            CREATE TABLE IF NOT EXISTS test_table (
+                id UUID PRIMARY KEY,
+                value VARCHAR
+            )
+            "#,
+            )
+            .execute(&mut conn)
+            .await
+            .expect("Create table fail.");
+
+            PgContext {
+                pg_container,
+                connection_string,
+            }
+        }
     }
 }
-
